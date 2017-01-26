a08d3195d2b4d41bc00aa4ccf6f37910a3e4afaa
azogue <eugenio.panadero@gmail.com>
2017-01-26 20:56:38 +0100

    v 0.5.2 pep8

--- dataweb/__init__.py
+++ dataweb/__init__.py
@@ -2,7 +2,9 @@
 """
 DATAWEB: Gestión de datos recogidos en web de forma periódica.
 """
+# noinspection PyUnresolvedReferences
 from dataweb.classdataweb import DataWeb
+# noinspection PyUnresolvedReferences
 from dataweb.requestweb.__init__ import get_data_en_intervalo, request_data_url
 
 
